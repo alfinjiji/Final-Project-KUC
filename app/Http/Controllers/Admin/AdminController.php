1be800ab07937e96ad1bac6c0a10ae9370cf8518
aswanthsreelakm <aswanthsreelakam@gmai.com>
2021-03-29 22:21:37 +0530

    confilit fixed again and again

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -15,44 +15,6 @@ class AdminController extends Controller
     {
         return view('admin.dashboard');
     }
-<<<<<<< HEAD
-   
-    
-    function product()
-    {
-        return view('admin.product');
-    }
-    function productEdit()
-    {
-        return view('admin.product_edit');
-    }
-   
-=======
-    function banner()
-    {
-        return view('admin.banner');
-    }
-    function bannerEdit()
-    {
-        return view('admin.banner_edit');
-    }
-    function customer()
-    {
-        return view('admin.customer');
-    }
-    function customerAddress()
-    {
-        return view('admin.customer_address');
-    }
-    function wishlist()
-    {
-        return view('admin.wishlist');
-    }
-    function customerOrder()
-    {
-        return view('admin.customer_order');
-    }
->>>>>>> b0ad5c778a6e1d263df8fdecc6b76826f58a180d
     function order()
     {
         return view('admin.order');
@@ -61,22 +23,6 @@ class AdminController extends Controller
     {
         return view('admin.order_product');
     }
-    function menu()
-    {
-        return view('admin.menu');
-    }
-    function menuCreate()
-    {
-        return view('admin.menu_create');
-    }
-    function menuEdit()
-    {
-        return view('admin.menu_edit');
-    }
     
     
-    function coupon()
-    {
-        return view('admin.coupon');
-    }
 }
